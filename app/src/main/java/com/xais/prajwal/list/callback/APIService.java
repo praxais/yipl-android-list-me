22aad47a58bb172338584e7b6e4394735d9c5ef2
Prajwal <praxais104@gmail.com>
2016-11-21 15:07:28 +0545

    APIService for comment added

--- app/src/main/java/com/xais/prajwal/list/callback/APIService.java
+++ app/src/main/java/com/xais/prajwal/list/callback/APIService.java
@@ -1,11 +1,13 @@
 package com.xais.prajwal.list.callback;
 
+import com.xais.prajwal.list.pojo.CommentPojo;
 import com.xais.prajwal.list.pojo.ListPojo;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Prajwal on 2016-11-21.
@@ -15,5 +17,8 @@ public interface APIService {
 
     @GET("posts")
     Call<List<ListPojo>> getAllList();
+
+    @GET("posts/{id}/comments")
+    Call<List<CommentPojo>> getIdComments(@Path("id") int id);
 
 }
